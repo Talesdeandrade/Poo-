c41be5dd5e9f952d23171f4067e5d3e802d57094
Talesdeandrade <talesdeandrade@gmail.com>
2023-05-10 21:09:55 -0300

    update1

--- app/Main.py
+++ app/Main.py
@@ -1,1 +1,16 @@
-print("Teste do projeto")
+class Main:
+    pass
+
+
+from Cliente import Cliente
+from Conta import Conta
+
+c1= Cliente("Joao","1197652300")
+conta=Conta(c1.nome,15400,100)
+#imprimeindo os valores
+#print(c1)
+print(c1.nome)
+print("Telefone do ",c1.nome,":",c1.telefone)
+print("Nome do titular: ", conta.titular)
+print("Numero da conta:",conta.numero)
+print("Saldo dísponível: R$ ",conta.saldo)
